a04772b1cefc795625b7b7689c7b52494765dcf9
Alexander Myasnikov <dart69@vk.com>
2026-02-05 18:22:44 +0300

    Add wayland session basic support.

--- 1c-client/run_launcher.sh
+++ 1c-client/run_launcher.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+groupmod -g "$HOST_GID" grp1cv8
+usermod -u "$HOST_UID" -g "$HOST_GID" usr1cv8
+chown -R "$HOST_UID":"$HOST_GID" /1c_user_home/
+
 exec gosu usr1cv8 /1c_dir/1cv8s
